870caf3a117cc25d8ca8f66c131df14cbf110916
Anjelica <ambrosio.anjelica@gmail.com>
2020-03-05 21:01:08 -0800

    updated

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -137,7 +137,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('127.0.0.1', 6379), ('mochi-mochi-aa.herokuapp.com', 10482)],
         },
     },
 }
